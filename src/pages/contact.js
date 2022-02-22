9f198048078c6b679516475a0b5d4e100ad210f2
BrainJarProductions <vgrilli1@gmail.com>
2022-02-21 23:43:59 -0500

    build contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,85 @@
 import React from 'react';
+import styled from 'styled-components';
+import SEO from '../components/SEO';
+import StoreLayout from '../components/StoreLayout';
+import useForm from '../utils/useForm';
+
+const FormStyles = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  fieldset {
+    grid-column: span 2;
+    max-height: 600px;
+    overflow: auto;
+    display: grid;
+    gap: 1rem;
+    align-content: start;
+  }
+  .mapleSyrup {
+    display: none;
+  }
+  .submit {
+    margin: auto;
+  }
+`;
 
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+    message: '',
+    mapleSyrup: '',
+  });
+
+  return (
+    <StoreLayout>
+      <SEO title="Home" keywords={[`wafn`, `offical`, `home`]} />
+      <FormStyles>
+        <fieldset>
+          <legend>Contact WAFN</legend>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={values.name}
+              onChange={updateValue}
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={values.email}
+              onChange={updateValue}
+            />
+          </label>
+          <label htmlFor="Message">
+            Message
+            <textarea
+              type="text"
+              name="message"
+              id="message"
+              value={values.message}
+              onChange={updateValue}
+            />
+          </label>
+          <input
+            type="mapleSyrup"
+            name="mapleSyrup"
+            id="mapleSyrup"
+            value={values.mapleSyrup}
+            onChange={updateValue}
+            className="mapleSyrup"
+          />
+          {/* TODO: Handle submit */}
+          <input className="submit" type="submit" value="Submit" />
+        </fieldset>
+      </FormStyles>
+    </StoreLayout>
+  );
 }
